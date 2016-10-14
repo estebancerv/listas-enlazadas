6bf3c6607856ac72df57319140a2d1a05776ea90
estebancerv <esteban.cervantes@ucsp.edu.pe>
2016-10-14 10:02:16 -0500

    Add files via upload

--- listas enlazadas.c
+++ listas enlazadas.c
@@ -1,9 +1,26 @@
+#include<stdio.h>
+
 struct node {
 	int val;
 	struct node *next;
 };
 
-struct node crearlista(struct node *cabeza){
+struct node crearlista(int n, struct node *cabeza){
+	int i;
+	struct node *med;
+	struct node *fin;
+	cabeza->val=1;
+	fin=cabeza;
+	for(i=2; i<=n; i++){
+		med = malloc(sizeof(struct node));
+		med->val=i;
+		fin->next=med;
+		fin=med;
+	}
+	fin->next= '\0';
+}
+
+/*struct node crearlista(struct node *cabeza){
 	struct node *medio, *final;
 	medio=malloc(sizeof(struct node));
 	final=malloc(sizeof(struct node));
@@ -14,7 +31,7 @@ struct node crearlista(struct node *cabeza){
 	medio->next=final;
 	final->next= '\0';
 	return *cabeza;
-}
+}*/
 
 void printlista(struct node *cabeza){
 	struct node *t;
@@ -26,27 +43,37 @@ void printlista(struct node *cabeza){
 	}
 }
 
-/*struct node borrar(int ele, struct node *cabeza){
-	int cont=0;
-	struct node *t;
-	t=malloc(sizeof(struct node));
-	t=cabeza;
-	while (t!='\0'){
-		if (cont=ele){
-			t->next=t->next->next;
+struct node borrar(int ele, struct node *cabeza){
+	struct node *fin;
+	struct node *med;
+	struct node *temp;
+	if (cabeza->val==ele){
+		temp=cabeza;
+		cabeza=cabeza->next;
+		free(temp);
+	}
+	else{
+		med=cabeza;
+		fin=cabeza->next;
+		while (fin!='\0'){
+			if (fin->val==ele){
+				med->next=fin->next;
+				free(fin);
+				break;
+			}
+			med=fin;
+			fin=fin->next;
 		}
-		t=t->next;
-		cont++;
 	}
 	return *cabeza;
-}*/
+}
 
 main() {
 	struct node *a;
 	a=malloc(sizeof(struct node));
-	crearlista(a);
+	crearlista(4,a);
 	printlista(a);
-	/*borrar(1,a);*/
+	borrar(3,a);
 	printlista(a);
 	return 0;
 }
